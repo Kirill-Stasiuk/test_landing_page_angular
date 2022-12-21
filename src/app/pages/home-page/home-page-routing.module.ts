baca2c966aeef8abe20dff25afa4e9fe6cd3c1af
Kiril <stasiuk2003@gmail.com>
2022-12-22 01:14:41 +0200

    Add: home-page component

--- src/app/pages/home-page/home-page-routing.module.ts
+++ src/app/pages/home-page/home-page-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
-import { HomePageComponent } from "./pages/home-page/components/home-page/home-page.component"
+import { HomePageComponent } from "./components/home-page/home-page.component"
 
 const routes: Routes = [{ path: "", component: HomePageComponent }]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class HomePageRoutingModule {}
