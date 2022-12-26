bce75f73b05838cdc7172dbb5f66dcb3ca7b2289
Kiril <stasiuk2003@gmail.com>
2022-12-26 14:41:11 +0200

    Add: footer

---
+++ src/app/shared/models/footer.model.ts
@@ -0,0 +1,9 @@
+export interface FooterLinks {
+  readonly text: string
+  readonly link: string
+}
+
+export interface SocialMedia {
+  readonly alt: string
+  readonly img: string
+}
---
+++ src/app/shared/statics/footer.static.ts
@@ -0,0 +1,39 @@
+import { FooterLinks, SocialMedia } from "../models/footer.model"
+
+export const FOOTER_LINKS: FooterLinks[] = [
+  {
+    text: "About",
+    link: "/",
+  },
+  {
+    text: "Services",
+    link: "/services",
+  },
+  {
+    text: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    text: "Pricing",
+    link: "/pricing",
+  },
+  {
+    text: "Testimonials",
+    link: "/",
+  },
+]
+
+export const SOCIAL_MEDIA: SocialMedia[] = [
+  {
+    alt: "twitter",
+    img: "/assets/images/twitter.svg",
+  },
+  {
+    alt: "linkedin",
+    img: "/assets/images/likedin.svg",
+  },
+  {
+    alt: "facebook",
+    img: "/assets/images/facebook.svg",
+  },
+]
