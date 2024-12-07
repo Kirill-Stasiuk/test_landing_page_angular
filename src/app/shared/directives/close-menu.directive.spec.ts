b7cafba3630c13814e8576daabcd29a8733dabac
KirillStasiuk <stasiuk2003@gmail.com>
2024-12-07 14:23:25 +0200

    fix tests

--- src/app/shared/directives/close-menu.directive.spec.ts
+++ src/app/shared/directives/close-menu.directive.spec.ts
@@ -1,8 +1,14 @@
 import { CloseMenuDirective } from './close-menu.directive';
+import { ElementRef } from "@angular/core"
+
+class MockElementRef implements ElementRef {
+  nativeElement = {};
+}
 
 describe('CloseMenuDirective', () => {
   it('should create an instance', () => {
-    const directive = new CloseMenuDirective();
+    const mockElementRef = new MockElementRef();
+    const directive = new CloseMenuDirective(mockElementRef);
     expect(directive).toBeTruthy();
   });
 });
